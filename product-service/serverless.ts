e3a041ac4dcd8e5a4b8b165a36e22216aebaab20
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-11-01 16:10:22 +0300

    remove unused plugins, remove comments

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -3,9 +3,6 @@ import type { Serverless } from 'serverless/aws';
 const serverlessConfiguration: Serverless = {
   service: {
     name: 'product-service',
-    // app and org for use with dashboard.serverless.com
-    // app: your-app-name,
-    // org: your-org-name,
   },
   frameworkVersion: '2',
   custom: {
@@ -13,15 +10,8 @@ const serverlessConfiguration: Serverless = {
       webpackConfig: './webpack.config.js',
       includeModules: true
     },
-    documentation: {
-      version: '1',
-      title: 'products API',
-      description: 'products API for craft beer online shop',
-      models: {},
-    }
   },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack', 'serverless-openapi-documentation'],
+  plugins: ['serverless-webpack', 'serverless-aws-documentation'],
   provider: {
     name: 'aws',
     runtime: 'nodejs12.x',
@@ -55,19 +45,6 @@ const serverlessConfiguration: Serverless = {
             method: 'get',
             path: 'products/{productId}',
             cors: true,
-/*             documentation: {
-              summary: "Get products",
-              description: "Get product by specific id",
-              pathParams: {
-                productId: {
-                  description: "The id of the product",
-                  scema: {
-                    type: "string",
-                    pattern: "^[-a-z0-9_]+$",
-                  }
-                }
-              }
-            } */
           }
         },
       ]
