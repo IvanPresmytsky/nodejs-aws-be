3b238607694930a01db24e8baa959e7ecd93ec92
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-12-12 23:56:19 +0300

    modify solution

--- bff-service/src/app.ts
+++ bff-service/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { Method } from 'axios';
+import { StatusCodes } from 'http-status-codes';
 
-import { runProxy, getServiceUrl, getServiceName } from './utils';
+import { getServiceUrl, getServiceName } from './utils';
 import { CacheService, ECacheFields } from './services/cacheService';
 
 dotenv.config({
@@ -24,16 +25,15 @@ app.all('/*', async (req, res) => {
   const serviceUrl = getServiceUrl(originalUrl);
 
   if (!serviceUrl) {
-    res.status(502).json({ error: 'Cannot process request' });
+    res.status(StatusCodes.BAD_GATEWAY).json({ error: 'Cannot process request' });
   }
 
   const finalURL = `${serviceUrl}${originalUrl}`;
 
   try {
-    const { status, data } = await runProxy(finalURL, method as Method, body);
-    const response = cacheService.checkForCached(serviceName, method as Method, data);
+    const { status, data } = await cacheService.runCachedProxy(serviceName, finalURL, method as Method, body);
 
-    res.status(status).json(response);
+    res.status(status).json(data);
   } catch (error) {
     const errorMessage = `Something went wrong! ${JSON.stringify(error)}`
     console.error(errorMessage);
@@ -41,7 +41,7 @@ app.all('/*', async (req, res) => {
     const { response } = error;
 
     if (!response) {
-      res.status(500).json(errorMessage);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorMessage);
     }
 
     res.status(response.status).json(response.data);
--- bff-service/src/services/cacheService.ts
+++ bff-service/src/services/cacheService.ts
@@ -1,5 +1,8 @@
 import { Method } from 'axios';
+import { StatusCodes } from 'http-status-codes';
+
 import { TProducts } from '../types';
+import { runProxy } from '../utils';
 
 export enum ECacheFields {
   Products = 'products'
@@ -9,69 +12,87 @@ export type TCachedDataTypes = TProducts;
 
 export type TCacheType = {
   products: {
-    data: TProducts;
-    isCached: boolean;
-    methods: Method[];
+    GET: {
+      data: TProducts;
+      isCached: boolean;
+      status: StatusCodes | null;
+      url: string;
+    }
   }
 };
 
 export class CacheService {
-  cache: TCacheType;
+  private cache: TCacheType;
 
-  timer: number;
+  private timer: number;
 
   constructor(cacheTimer = 0) {
     this.timer = cacheTimer;
     this.cache = {
       products: {
-        data: [],
-        isCached: false,
-        methods: ['GET'],
+        GET: {
+          data: [],
+          isCached: false,
+          status: null,
+          url: '',
+        }
       },
     }
   }
 
-  private getCache(cacheField: ECacheFields) {
-    return this.cache[cacheField].data;
+  private clearCache(cacheField: ECacheFields, method: Method) {
+    this.cache[cacheField][method] = {
+      isCached: false,
+      status: null,
+      url: '',
+      data: [],
+    }
   }
 
-  private getIsCached(cacheField: ECacheFields) {
-    return this.cache[cacheField].isCached;
-  }
+  private setCache(
+    cacheField: ECacheFields,
+    url: string,
+    method: Method,
+    data: TCachedDataTypes,
+    status: StatusCodes
+  ) {
+    if (!this.cache[cacheField]?.[method]) {
+      return;
+    }
 
-  private setCache(cacheField: ECacheFields, data: TCachedDataTypes) {
-    this.cache[cacheField].data = data;
-    this.cache[cacheField].isCached = true;
+    this.cache[cacheField][method] = {
+      isCached: true,
+      status,
+      url,
+      data,
+    };
 
     setTimeout(() => {
-      this.cache[cacheField].isCached = false;
-      console.log(`Caching for ${cacheField} was switched off`);
+      this.clearCache(cacheField, method);
+      console.log(`Caching for ${cacheField} for method ${method} was switched off`);
     }, this.timer);
   }
 
-  private cacheData(cacheField: ECacheFields, data: TCachedDataTypes) {
-    if (!this.cache[cacheField]) {
-      throw new Error(`Field ${cacheField} doesn't presented in CacheService!`)
-    }
-
-    if (this.getIsCached(cacheField)) {
-      return this.getCache(cacheField);
-    }
-
-    this.setCache(cacheField, data);
-
-    return data;
+  private isCached(cacheField: ECacheFields, url: string, method: Method) {
+    return this.cache[cacheField]?.[method]
+      && this.cache[cacheField][method].url === url
+      && this.cache[cacheField][method].isCached;
   }
 
-  checkForCached(cacheField: ECacheFields, method: Method, data: TCachedDataTypes) {
-    if (!this.cache[cacheField]) {
-      throw new Error(`Field ${cacheField} doesn't presented in CacheService!`);
+  async runCachedProxy(cacheField: ECacheFields, url: string, method: Method, body: any) {
+    const isCached = this.isCached(cacheField, url, method);
+    if (isCached) {
+      console.log('Sending cached data...');
+      return {
+        status: this.cache[cacheField][method].status,
+        data: this.cache[cacheField][method].data,
+      }
     }
 
-    if (this.cache[cacheField].methods.includes(method)) {
-      return this.cacheData(cacheField, data)
-    }
+    const { status, data } = await runProxy(url, method as Method, body);
 
-    return data;
+    this.setCache(cacheField, url, method, data, status);
+
+    return { status, data };
   }
 }
